6c42e736fcc8c6a7707b20e03598acff3fae34c1
ShenHuaJie <taoyixuelang@163.com>
2016-04-06 14:32:44 +0800

    druid

---
+++ src/main/java/org/shjr/iplat/core/config/DBPasswordCallback.java
@@ -0,0 +1,30 @@
+package org.shjr.iplat.core.config;
+
+import java.util.Properties;
+
+import org.apache.commons.lang3.StringUtils;
+import org.shjr.iplat.core.util.SecurityUtil;
+
+import com.alibaba.druid.util.DruidPasswordCallback;
+
+@SuppressWarnings("serial")
+public class DBPasswordCallback extends DruidPasswordCallback {
+	static final byte[] key = { 9, -1, 0, 5, 39, 8, 6, 19 };
+
+	public void setProperties(Properties properties) {
+		super.setProperties(properties);
+		String pwd = properties.getProperty("password");
+		if (StringUtils.isNotBlank(pwd)) {
+			try {
+				String password = SecurityUtil.decryptDes(pwd, key);
+				setPassword(password.toCharArray());
+			} catch (Exception e) {
+				setPassword(pwd.toCharArray());
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		System.out.println(SecurityUtil.encryptDes("", key));
+	}
+}
