5336629a52f1168ea34efb9a9df8971b55faecee
ShenHuaJie <taoyixuelang@163.com>
2016-04-18 17:46:45 +0800

    P

--- src/main/java/org/ibase4j/core/interceptor/LoginInterceptor.java
+++ src/main/java/org/ibase4j/core/interceptor/LoginInterceptor.java
@@ -1,21 +1,14 @@
 package org.ibase4j.core.interceptor;
 
-import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.InetAddress;
-import java.util.Enumeration;
-import java.util.Properties;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.ibase4j.core.support.HttpCode;
 import org.ibase4j.core.util.WebUtil;
-import org.springframework.core.io.support.PropertiesLoaderUtils;
-import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 /**
  * 登录拦截器
@@ -23,38 +16,18 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
  * @author ShenHuaJie
  * @version 2016年4月2日 下午4:19:47
  */
-public class HandlerInterceptor extends HandlerInterceptorAdapter {
-	private static Logger logger = LogManager.getLogger(HandlerInterceptor.class);
+public class LoginInterceptor extends BaseInterceptor {
 
-	private static String[] notFilter = new String[] {};
-	static {
-		try {
-			String url = "";
-			Properties properties = PropertiesLoaderUtils.loadAllProperties("classpath:config/whiteURL.properties");
-			for (Enumeration<?> iterator = properties.keys(); iterator.hasMoreElements();) {
-				String key = (String) iterator.nextElement();
-				url += "," + properties.getProperty(key) + ",";
-			}
-			notFilter = url.split(",");
-		} catch (IOException e) {
-			logger.error("", e);
-		}
-	}
-
-	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
-		boolean success = true;
-		String reqUri = request.getRequestURI();
 		String url = request.getServletPath();
 		logger.info(url);
-		for (String s : notFilter) { // 如果uri中包含不过滤的uri，则不进行过滤
-			if (reqUri.indexOf(s) != -1 && StringUtils.isNotBlank(s)) {
-				return success;
-			}
+		if (super.whiteURL(request)) {
+			return true;
 		}
+		boolean success = true;
 		String curruser = WebUtil.getCurrentUser(request);
-		if (reqUri.indexOf("online") == -1) { // 后端请求后端数据
+		if (url.indexOf("online") == -1) { // 后端请求后端数据
 			if (curruser == null) {
 				success = false;
 				response.setStatus(HttpCode.HTTP_CODE_401);
