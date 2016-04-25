b907afaccbf7a495f312514017ca13a4e839c6df
ShenHuaJie <taoyixuelang@163.com>
2016-04-25 08:31:23 +0800

    ftp

--- src/main/java/org/ibase4j/core/support/ftp/FileProgressMonitor.java
+++ src/main/java/org/ibase4j/core/support/ftp/FileProgressMonitor.java
@@ -12,10 +12,10 @@ public class FileProgressMonitor implements SftpProgressMonitor {
 	private Logger logger = LogManager.getLogger();
 	private long transfered; // 记录已传输的数据总大小
 	private long fileSize; // 记录文件总大小
-	private int minInterval = 6; // 打印日志时间间隔
+	private int minInterval = 100; // 打印日志时间间隔
 	private long start; // 开始时间
 	private DecimalFormat df = new DecimalFormat("#.##");
-	private double cacheProgress; //
+	private long preInterval;
 
 	public FileProgressMonitor(long fileSize) {
 		this.fileSize = fileSize;
@@ -32,16 +32,15 @@ public class FileProgressMonitor implements SftpProgressMonitor {
 	public boolean count(long count) {
 		if (fileSize != 0 && transfered == 0) {
 			logger.info("Sending progress message: {}%", df.format(0));
+			preInterval = System.currentTimeMillis();
 		}
 		add(count);
 		if (fileSize != 0) {
-			double ad = ((double) count * 100) / (double) fileSize;
-			if (transfered == fileSize || cacheProgress > minInterval) {
-				cacheProgress = 0;
+			long interval = System.currentTimeMillis() - preInterval;
+			if (transfered == fileSize || interval > minInterval) {
+				preInterval = System.currentTimeMillis();
 				double d = ((double) transfered * 100) / (double) fileSize;
 				logger.info("Sending progress message: {}%", df.format(d));
-			} else {
-				cacheProgress += ad;
 			}
 		} else {
 			logger.info("Sending progress message: " + transfered);
@@ -53,7 +52,7 @@ public class FileProgressMonitor implements SftpProgressMonitor {
 	 * 实现了SftpProgressMonitor接口的end方法
 	 */
 	public void end() {
-		logger.info("transfering end.use time: {}ms", System.currentTimeMillis() - start);
+		logger.info("Transferring end. used time: {}ms", System.currentTimeMillis() - start);
 	}
 
 	private void add(long count) {
