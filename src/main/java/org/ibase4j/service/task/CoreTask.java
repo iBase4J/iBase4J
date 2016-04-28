87180af62075b55f027a18ea91d7edc819ad9c0f
ShenHuaJie <taoyixuelang@163.com>
2016-04-28 17:50:53 +0800

    Task

--- src/main/java/org/ibase4j/service/task/CoreTask.java
+++ src/main/java/org/ibase4j/service/task/CoreTask.java
@@ -3,15 +3,13 @@ package org.ibase4j.service.task;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ibase4j.core.config.Resources;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-@Component
+@Component("coreTask")
 public class CoreTask {
 	Logger logger = LogManager.getLogger();
 
 	/** 定时清除国际化信息 */
-	@Scheduled(cron = "0 0/30 * * * ?")
 	public void run() {
 		Resources.flushMessage();
 		logger.info("Messages flushed!");
