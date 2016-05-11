06a90054253f4b009a2f6cf7332ed54a35b3c283
ShenHuaJie <taoyixuelang@163.com>
2016-05-11 13:51:01 +0800

    权限

--- src/main/java/org/ibase4j/core/interceptor/LocaleInterceptor.java
+++ src/main/java/org/ibase4j/core/interceptor/LocaleInterceptor.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.apache.commons.lang3.StringUtils;
+import org.ibase4j.core.support.HttpCode;
 import org.springframework.context.i18n.LocaleContextHolder;
 
 /**
@@ -20,8 +21,27 @@ public class LocaleInterceptor extends BaseInterceptor {
 			throws Exception {
 		String url = request.getServletPath();
 		logger.info(url);
-		// 设置客户端语言
 		HttpSession session = request.getSession();
+		Long preRequestTime = (Long) session.getAttribute("PREREQUESTTIME");
+		if (preRequestTime != null) { // 过滤频繁操作
+			if (System.currentTimeMillis() - preRequestTime < 1000) {
+				Integer illRequestTimes = (Integer) session.getAttribute("ILLREQUESTTIMES");
+				if (illRequestTimes == null) {
+					illRequestTimes = 1;
+				} else {
+					illRequestTimes++;
+				}
+				if (illRequestTimes > 2) {
+					response.setStatus(HttpCode.CONFLICT.value());
+					return false;
+				}
+				session.setAttribute("ILLREQUESTTIMES", illRequestTimes);
+			} else {
+				session.setAttribute("ILLREQUESTTIMES", 0);
+			}
+		}
+		session.setAttribute("PREREQUESTTIME", System.currentTimeMillis());
+		// 设置客户端语言
 		Locale locale = (Locale) session.getAttribute("LOCALE");
 		if (locale == null) {
 			String language = request.getParameter("locale");
@@ -33,25 +53,6 @@ public class LocaleInterceptor extends BaseInterceptor {
 			}
 		}
 		LocaleContextHolder.setLocale(locale);
-		Long preRequestTime = (Long) session.getAttribute("PREREQUESTTIME");
-		if (preRequestTime != null) { // 过滤频繁操作
-			if (System.currentTimeMillis() - preRequestTime < 1000) {
-				Integer illRequestTimes = (Integer) session.getAttribute("ILLREQUESTTIMES");
-				if (illRequestTimes == null) {
-					illRequestTimes = 1;
-				} else {
-					illRequestTimes++;
-				}
-				if (illRequestTimes > 2) {
-					response.setStatus(409);
-					return false;
-				}
-				session.setAttribute("ILLREQUESTTIMES", illRequestTimes);
-			} else {
-				session.setAttribute("ILLREQUESTTIMES", 0);
-			}
-		}
-		session.setAttribute("PREREQUESTTIME", System.currentTimeMillis());
 		return nextInterceptor(request, response, handler);
 	}
 }
