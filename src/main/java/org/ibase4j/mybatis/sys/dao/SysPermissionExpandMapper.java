22256c1eb91f1f4fdb69f229a01744d043527c8d
ShenHuaJie <taoyixuelang@163.com>
2016-05-11 17:40:39 +0800

    权限

--- src/main/java/org/ibase4j/mybatis/sys/dao/SysPermissionExpandMapper.java
+++ src/main/java/org/ibase4j/mybatis/sys/dao/SysPermissionExpandMapper.java
@@ -1,11 +1,8 @@
 package org.ibase4j.mybatis.sys.dao;
 
-import java.util.List;
-
 import org.apache.ibatis.annotations.Param;
 
 public interface SysPermissionExpandMapper {
-	List<String> getPermissionTypesByUserId(Integer userId);
 
 	Integer getPermissionByUserId(@Param("userId") Integer userId, @Param("url") String url);
 }
--- src/main/java/org/ibase4j/service/sys/SysPermissionService.java
+++ src/main/java/org/ibase4j/service/sys/SysPermissionService.java
@@ -1,7 +1,5 @@
 package org.ibase4j.service.sys;
 
-import java.util.List;
-
 import org.ibase4j.mybatis.sys.dao.SysPermissionExpandMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
@@ -19,11 +17,6 @@ public class SysPermissionService {
 
 	@Autowired
 	private SysPermissionExpandMapper sysPermissionExpandMapper;
-
-	@Cacheable
-	public List<String> getPermissionTypesByUserId(Integer userId) {
-		return sysPermissionExpandMapper.getPermissionTypesByUserId(userId);
-	}
 
 	@Cacheable
 	public boolean doCheckPermissionByUserId(Integer userId, String url) {
