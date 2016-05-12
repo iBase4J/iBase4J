2fec5ede95ac716ff6476d4748abadcecc39a258
ShenHuaJie <taoyixuelang@163.com>
2016-05-12 17:47:08 +0800

    会话管理

--- src/main/java/org/ibase4j/service/sys/SysSessionService.java
+++ src/main/java/org/ibase4j/service/sys/SysSessionService.java
@@ -14,8 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.CacheConfig;
 import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.github.pagehelper.Page;
 import com.github.pagehelper.PageInfo;
 
