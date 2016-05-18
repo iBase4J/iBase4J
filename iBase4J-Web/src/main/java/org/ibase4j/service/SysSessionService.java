f6235a65a32b2333cfc70d723be581779558647b
ShenHuaJie <taoyixuelang@163.com>
2016-05-18 17:22:18 +0800

    会话管理

--- iBase4J-Web/src/main/java/org/ibase4j/service/SysSessionService.java
+++ iBase4J-Web/src/main/java/org/ibase4j/service/SysSessionService.java
@@ -2,16 +2,11 @@ package org.ibase4j.service;
 
 import java.util.Map;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.apache.shiro.SecurityUtils;
-import org.apache.shiro.session.Session;
-import org.apache.shiro.session.mgt.DefaultSessionKey;
-import org.apache.shiro.session.mgt.SessionKey;
 import org.ibase4j.core.config.Resources;
 import org.ibase4j.facade.sys.SysSessionFacade;
 import org.ibase4j.mybatis.generator.model.SysSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.session.data.redis.RedisOperationsSessionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -19,9 +14,10 @@ import com.github.pagehelper.PageInfo;
 
 @Service
 public class SysSessionService {
-	private final Logger logger = LogManager.getLogger();
 	@Autowired
 	private SysSessionFacade sysSessionFacade;
+	@Autowired
+	private RedisOperationsSessionRepository sessionRepository;
 
 	public PageInfo<?> query(Map<String, Object> params) {
 		return sysSessionFacade.query(params);
@@ -31,17 +27,8 @@ public class SysSessionService {
 		Assert.notNull(id, Resources.getMessage("ID_IS_NULL"));
 		SysSession sysSession = sysSessionFacade.queryById(id);
 		if (sysSession != null) {
-			try {
-				SessionKey sessionKey = new DefaultSessionKey(sysSession.getSessionId());
-				Session session = SecurityUtils.getSecurityManager().getSession(sessionKey);
-				if (session != null) {
-					session.stop();
-				}
-			} catch (Exception e) {
-				logger.error(e);
-			}
+			sessionRepository.delete(sysSession.getSessionId());
 		}
-		sysSessionFacade.delete(id);
 	}
 
 	public void update(SysSession record) {
