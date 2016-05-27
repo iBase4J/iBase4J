111ebc0f847641c9ada346a4f961b03612803982
ShenHuaJie <taoyixuelang@163.com>
2016-05-27 09:44:25 +0800

    Server

---
+++ iBase4J-Scheduler/src/main/java/org/ibase4j/scheduler/App.java
@@ -0,0 +1,13 @@
+package org.ibase4j.scheduler;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
