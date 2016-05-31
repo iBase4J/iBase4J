ea26383c6dcdaba0acf918c1be7d96e4d197d896
ShenHuaJie <taoyixuelang@163.com>
2016-05-31 12:00:52 +0800

    调度

--- iBase4J-Common/src/main/java/org/ibase4j/core/Constants.java
+++ iBase4J-Common/src/main/java/org/ibase4j/core/Constants.java
@@ -22,9 +22,11 @@ public final class Constants {
 	public static final String ALLUSER_NUMBER = "ALLUSER_NUMBER";
 	/** 登录用户数量 */
 	public static final String USER_NUMBER = "USER_NUMBER";
-
+	/** 上次请求地址 */
 	public static final String PREREQUEST = "PREREQUEST";
+	/** 上次请求时间 */
 	public static final String PREREQUEST_TIME = "PREREQUEST_TIME";
+	/** 非法请求次数 */
 	public static final String MALICIOUS_REQUEST_TIMES = "MALICIOUS_REQUEST_TIMES";
 
 }
