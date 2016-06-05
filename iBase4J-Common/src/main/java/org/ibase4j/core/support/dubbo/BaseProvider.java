2003c8f318f26fa9810079e8f8581f835390ac17
ShenHuaJie <taoyixuelang@163.com>
2016-06-05 21:30:18 +0800

    事务

--- iBase4J-Common/src/main/java/org/ibase4j/core/support/dubbo/BaseProvider.java
+++ iBase4J-Common/src/main/java/org/ibase4j/core/support/dubbo/BaseProvider.java
@@ -2,6 +2,8 @@ package org.ibase4j.core.support.dubbo;
 
 import java.util.Map;
 
+import org.springframework.transaction.annotation.Transactional;
+
 import com.github.pagehelper.PageInfo;
 
 /**
@@ -9,8 +11,10 @@ import com.github.pagehelper.PageInfo;
  * @version 2016年5月20日 下午3:19:19
  */
 public interface BaseProvider<K> {
+	@Transactional
 	public K update(K record);
 
+	@Transactional
 	public void delete(Integer id);
 
 	public K queryById(Integer id);
--- iBase4J-SYS-API/src/main/java/org/ibase4j/provider/sys/SysSessionProvider.java
+++ iBase4J-SYS-API/src/main/java/org/ibase4j/provider/sys/SysSessionProvider.java
@@ -5,12 +5,13 @@ package org.ibase4j.provider.sys;
 
 import org.ibase4j.core.support.dubbo.BaseProvider;
 import org.ibase4j.mybatis.generator.model.SysSession;
+import org.springframework.transaction.annotation.Transactional;
 
 /**
  * @author ShenHuaJie
  * @version 2016年5月15日 上午11:21:21
  */
 public interface SysSessionProvider extends BaseProvider<SysSession> {
-
+	@Transactional
 	public void deleteBySessionId(final String sessionId);
 }
