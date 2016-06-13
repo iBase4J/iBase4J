6b1f3af2bec6fcc31a82d9d91d58bcaa3f15569f
ShenHuaJie <taoyixuelang@163.com>
2016-06-13 17:40:50 +0800

    优化

--- iBase4J-Web/src/main/java/org/ibase4j/service/sys/SysPermissionService.java
+++ iBase4J-Web/src/main/java/org/ibase4j/service/sys/SysPermissionService.java
@@ -1,11 +1,10 @@
 package org.ibase4j.service.sys;
 
-import org.ibase4j.core.Constants;
 import org.ibase4j.core.support.BaseService;
-import org.ibase4j.core.util.RedisUtil;
 import org.ibase4j.model.generator.SysPermission;
 import org.ibase4j.provider.sys.SysPermissionProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 /**
@@ -19,16 +18,8 @@ public class SysPermissionService extends BaseService<SysPermissionProvider, Sys
 		this.provider = provider;
 	}
 
+	@Cacheable("sysPermission")
 	public Boolean doCheckPermissionByUserId(Integer userId, String url) {
-		StringBuilder sb = new StringBuilder(Constants.CACHE_NAMESPACE);
-		String className = this.getClass().getSimpleName();
-		sb.append(className.substring(0, 1).toLowerCase())
-				.append(className.substring(1, className.length() - 7));
-		sb.append(":").append(userId).append(",").append(url);
-		byte[] value = RedisUtil.get(sb.toString().getBytes());
-		if (value != null) {
-			return (Boolean) valueSerializer.deserialize(value);
-		}
 		return provider.doCheckPermissionByUserId(userId, url);
 	}
 }
