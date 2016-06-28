b37f3e8e91a811b7a4fc0713a71c6402e97a0b18
ShenHuaJie <taoyixuelang@163.com>
2016-06-28 10:05:08 +0800

    调度

--- iBase4J-Scheduler-Service/src/main/java/org/ibase4j/scheduler/manager/TaskListener.java
+++ iBase4J-Scheduler-Service/src/main/java/org/ibase4j/scheduler/manager/TaskListener.java
@@ -24,7 +24,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.alibaba.fastjson.JSON;
 
 /**
- * 日志监听器
+ * 调度监听器
  * 
  * @author ShenHuaJie
  * @version 2016年5月27日 下午4:31:31
