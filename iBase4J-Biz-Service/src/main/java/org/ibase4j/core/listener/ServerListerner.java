9c625a9305b7a36086ef29ffd986ba036f249772
ShenHuaJie <taoyixuelang@163.com>
2016-08-14 09:05:35 +0800

    业务开发实例

---
+++ iBase4J-Biz-Service/src/main/java/org/ibase4j/core/listener/ServerListerner.java
@@ -0,0 +1,22 @@
+package org.ibase4j.core.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class ServerListerner implements ServletContextListener {
+    protected final Logger logger = LogManager.getLogger(this.getClass());
+
+    public void contextDestroyed(ServletContextEvent contextEvent) {
+    }
+
+    public void contextInitialized(ServletContextEvent contextEvent) {
+        // WebApplicationContext context = ContextLoader.getCurrentWebApplicationContext();
+
+        logger.info("=================================");
+        logger.info("系统[{}]启动完成!!!", contextEvent.getServletContext().getServletContextName());
+        logger.info("=================================");
+    }
+}
