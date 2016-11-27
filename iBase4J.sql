5afde5697cf30e3dc562c80b4b55e401400911f4
ShenHuaJie <taoyixuelang@163.com>
2016-11-27 15:13:58 +0800

    调度管理

--- iBase4J.sql
+++ iBase4J.sql
@@ -5,11 +5,6 @@
 -- HeidiSQL 版本:                  9.3.0.4984
 -- --------------------------------------------------------
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-
 -- 导出 ibase4j 的数据库结构
 DROP DATABASE IF EXISTS `ibase4j`;
 CREATE DATABASE IF NOT EXISTS `ibase4j` /*!40100 DEFAULT CHARACTER SET utf8 */;
