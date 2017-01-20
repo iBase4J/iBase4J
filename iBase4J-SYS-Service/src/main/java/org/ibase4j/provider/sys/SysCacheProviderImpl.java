0e80cc36d055777b5a5d3ea6aebeafbb48cce1a6
ShenHuaJie <taoyixuelang@163.com>
2017-01-20 09:35:18 +0800

    目录结果修改

--- iBase4J-SYS-Service/src/main/java/org/ibase4j/provider/sys/SysCacheProviderImpl.java
+++ iBase4J-SYS-Service/src/main/java/org/ibase4j/provider/sys/SysCacheProviderImpl.java
@@ -1,17 +1,21 @@
 package org.ibase4j.provider.sys;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.ibase4j.core.support.dubbo.spring.annotation.DubboService;
-import org.ibase4j.core.util.RedisUtil;
-import org.ibase4j.provider.sys.ISysCacheProvider;
+import org.ibase4j.core.util.RedissonUtil;
 
 @DubboService(interfaceClass = ISysCacheProvider.class)
 public class SysCacheProviderImpl implements ISysCacheProvider {
+	Logger logger = LogManager.getLogger();
 
 	// 清缓存
 	public void flush() {
-		RedisUtil.delAll("*:sysDics:*");
-		RedisUtil.delAll("*:sysDicMap:*");
-		RedisUtil.delAll("*:getAuthorize:*");
-		RedisUtil.delAll("*:sysPermission:*");
+		logger.info("清缓存开始......");
+		RedissonUtil.delAll("*:sysDics:*");
+		RedissonUtil.delAll("*:sysDicMap:*");
+		RedissonUtil.delAll("*:getAuthorize:*");
+		RedissonUtil.delAll("*:sysPermission:*");
+		logger.info("清缓存结束!");
 	}
 }
--- iBase4J-SYS-Web/src/main/java/org/ibase4j/service/sys/SysCacheService.java
+++ iBase4J-SYS-Web/src/main/java/org/ibase4j/service/sys/SysCacheService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SysCacheService {
-    @DubboReference
+	@DubboReference(async = true)
 	private ISysCacheProvider sysCacheProvider;
 
 	public void flushCache() {
