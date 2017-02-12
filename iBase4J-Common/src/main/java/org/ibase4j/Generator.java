2d50e64b7edcecb22956e485c646e3608f014d5b
ShenHuaJie <taoyixuelang@163.com>
2017-02-12 22:23:56 +0800

    统一DUBBO服务接口

--- iBase4J-Common/src/main/java/org/ibase4j/Generator.java
+++ iBase4J-Common/src/main/java/org/ibase4j/Generator.java
@@ -1,10 +1,6 @@
 package org.ibase4j;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import com.baomidou.mybatisplus.generator.AutoGenerator;
-import com.baomidou.mybatisplus.generator.InjectionConfig;
 import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
 import com.baomidou.mybatisplus.generator.config.GlobalConfig;
 import com.baomidou.mybatisplus.generator.config.PackageConfig;
@@ -15,6 +11,7 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 /**
  * 代码生成
+ * 
  * @author ShenHuaJie
  */
 public class Generator {
@@ -47,29 +44,30 @@ public class Generator {
 		dsc.setDbType(DbType.MYSQL);
 		dsc.setDriverName("com.mysql.jdbc.Driver");
 		dsc.setUsername("root");
-		dsc.setPassword("root");
+		dsc.setPassword("buzhidao");
 		dsc.setUrl("jdbc:mysql://127.0.0.1:3306/iBase4J?characterEncoding=utf8");
 		mpg.setDataSource(dsc);
 		// 策略配置
 		StrategyConfig strategy = new StrategyConfig();
-		strategy.setTablePrefix("sys_");// 此处可以修改为您的表前缀
+		// strategy.setTablePrefix("sys_");// 此处可以修改为您的表前缀
 		strategy.setNaming(NamingStrategy.underline_to_camel);// 表名生成策略
-		// strategy.setInclude(new String[] { "user" }); // 需要生成的表
+		strategy.setInclude(new String[] { "sys_news" }); // 需要生成的表
 		// strategy.setExclude(new String[]{"test"}); // 排除生成的表
 		// 字段名生成策略
 		strategy.setFieldNaming(NamingStrategy.underline_to_camel);
 		// 自定义实体父类
-		// strategy.setSuperEntityClass("com.baomidou.demo.TestEntity");
+		strategy.setSuperEntityClass("org.ibase4j.core.base.BaseModel");
 		// 自定义实体，公共字段
-		// strategy.setSuperEntityColumns(new String[] { "test_id", "age" });
+		strategy.setSuperEntityColumns(
+				new String[] { "id_", "enable_", "remark_", "create_by", "create_time", "update_by", "update_time" });
 		// 自定义 mapper 父类
-		// strategy.setSuperMapperClass("com.baomidou.demo.TestMapper");
+		strategy.setSuperMapperClass("org.ibase4j.core.base.BaseMapper");
 		// 自定义 service 父类
-		// strategy.setSuperServiceClass("com.baomidou.demo.TestService");
+		// strategy.setSuperServiceClass("org.ibase4j.core.base.BaseService");
 		// 自定义 service 实现类父类
-		// strategy.setSuperServiceImplClass("com.baomidou.demo.TestServiceImpl");
+		strategy.setSuperServiceImplClass("org.ibase4j.core.base.BaseService");
 		// 自定义 controller 父类
-		// strategy.setSuperControllerClass("com.baomidou.demo.TestController");
+		strategy.setSuperControllerClass("org.ibase4j.core.base.BaseController");
 		// 【实体】是否生成字段常量（默认 false）
 		// public static final String ID = "test_id";
 		// strategy.setEntityColumnConstant(true);
@@ -78,30 +76,26 @@ public class Generator {
 		// strategy.setEntityBuliderModel(true);
 		mpg.setStrategy(strategy);
 		// 包配置
+		String module = ".sys";
 		PackageConfig pc = new PackageConfig();
 		pc.setParent("org.ibase4j");
-		pc.setModuleName("sys");
+		pc.setEntity("model" + module);
+		pc.setMapper("dao" + module);
+		pc.setXml("dao" + module);
+		pc.setService("ignore");
+		pc.setServiceImpl("service" + module);
+		pc.setController("web" + module);
 		mpg.setPackageInfo(pc);
-		// 注入自定义配置，可以在 VM 中使用 cfg.abc 设置的值
-		InjectionConfig cfg = new InjectionConfig() {
-			@Override
-			public void initMap() {
-				Map<String, Object> map = new HashMap<String, Object>();
-				map.put("abc", this.getConfig().getGlobalConfig().getAuthor() + "-mp");
-				this.setMap(map);
-			}
-		};
-		mpg.setCfg(cfg);
 		// 自定义模板配置，可以 copy 源码 mybatis-plus/src/main/resources/template 下面内容修改，
 		// 放置自己项目的 src/main/resources/template 目录下, 默认名称一下可以不配置，也可以自定义模板名称
-		 TemplateConfig tc = new TemplateConfig();
-		 tc.setEntity("tpl/model.java.vm");
-		 tc.setMapper("tpl/dao.java.vm");
-		 tc.setXml("tpl/mapper.xml.vm");
-		 mpg.setTemplate(tc);
+		TemplateConfig tc = new TemplateConfig();
+		tc.setEntity("template/entity.java.vm");
+		tc.setMapper("template/mapper.java.vm");
+		tc.setXml("template/mapper.xml.vm");
+		tc.setServiceImpl("template/serviceImpl.java.vm");
+		tc.setController("template/controller.java.vm");
+		mpg.setTemplate(tc);
 		// 执行生成
 		mpg.execute();
-		// 打印注入设置
-		System.err.println(mpg.getCfg().getMap().get("abc"));
 	}
 }
