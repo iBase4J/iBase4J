b6e11e8f3826be1c65d7240fb9b3efbfa424425d
A♂圣旭 <huajieshen@126.com>
2017-03-26 12:41:23 +0800

    优化缓存

--- iBase4J-SYS-Service/src/main/java/org/ibase4j/service/SysCacheService.java
+++ iBase4J-SYS-Service/src/main/java/org/ibase4j/service/SysCacheService.java
@@ -1,21 +1,29 @@
 package org.ibase4j.service;
 
+import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.ibase4j.core.Constants;
 import org.ibase4j.core.util.CacheUtil;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SysCacheService {
-	Logger logger = LogManager.getLogger();
+    Logger logger = LogManager.getLogger();
 
-	// 清缓存
-	public void flush() {
-		logger.info("清缓存开始......");
-		CacheUtil.getCache().delAll("*:sysDics:*");
-		CacheUtil.getCache().delAll("*:sysDicMap:*");
-		CacheUtil.getCache().delAll("*:getAuthorize:*");
-		CacheUtil.getCache().delAll("*:sysPermission:*");
-		logger.info("清缓存结束!");
-	}
+    // 清缓存
+    public void flush() {
+        logger.info("清缓存开始......");
+        CacheUtil.getCache().delAll(Constants.CACHE_NAMESPACE + "*");
+        logger.info("清缓存结束!");
+    }
+
+    // 清缓存
+    public void flush(Map<String, String> param) {
+        String key = param.get("key");
+        logger.info("清缓存[{}]开始......", key);
+        CacheUtil.getCache().delAll(Constants.CACHE_NAMESPACE + "*" + key + "*");
+        logger.info("清缓存[{}]结束!", key);
+    }
 }
