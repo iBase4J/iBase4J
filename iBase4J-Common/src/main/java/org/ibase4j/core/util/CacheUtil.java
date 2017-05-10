cc7865085e57be8394ef2d1cd57ed2207f9f3f75
A♂圣旭 <huajieshen@126.com>
2017-05-10 11:26:25 +0800

    优化

--- iBase4J-Common/src/main/java/org/ibase4j/core/util/CacheUtil.java
+++ iBase4J-Common/src/main/java/org/ibase4j/core/util/CacheUtil.java
@@ -10,69 +10,72 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class CacheUtil {
-    private static Logger logger = LogManager.getLogger(CacheUtil.class);
-    private static CacheManager cacheManager;
-    private static RedisHelper redisHelper;
+	private static Logger logger = LogManager.getLogger(CacheUtil.class);
+	private static CacheManager cacheManager;
+	private static RedisHelper redisHelper;
 
-    @Bean
-    public CacheManager setCache() {
-        cacheManager = getCache();
-        return cacheManager;
-    }
+	@Bean
+	public CacheManager setCache() {
+		cacheManager = getCache();
+		return cacheManager;
+	}
 
-    public static CacheManager getCache() {
-        if (cacheManager == null) {
-            synchronized (CacheUtil.class) {
-                if (cacheManager == null) {
-                    cacheManager = new RedissonHelper();
-                }
-            }
-        }
-        return cacheManager;
-    }
+	public static CacheManager getCache() {
+		if (cacheManager == null) {
+			synchronized (CacheUtil.class) {
+				if (cacheManager == null) {
+					cacheManager = new RedissonHelper();
+				}
+			}
+		}
+		return cacheManager;
+	}
 
-    @Bean
-    public RedisHelper setRedisHelper() {
-        redisHelper = getRedisHelper();
-        return redisHelper;
-    }
+	@Bean
+	public RedisHelper setRedisHelper() {
+		redisHelper = getRedisHelper();
+		return redisHelper;
+	}
 
-    public static RedisHelper getRedisHelper() {
-        if (redisHelper == null) {
-            synchronized (CacheUtil.class) {
-                if (redisHelper == null) {
-                    redisHelper = new RedisHelper();
-                }
-            }
-        }
-        return redisHelper;
-    }
+	public static RedisHelper getRedisHelper() {
+		if (redisHelper == null) {
+			synchronized (CacheUtil.class) {
+				if (redisHelper == null) {
+					redisHelper = new RedisHelper();
+				}
+			}
+		}
+		return redisHelper;
+	}
 
-    /** 获取锁 */
-    public static boolean getLock(String key) {
-        try {
-            if (!getRedisHelper().exists(key)) {
-                synchronized (CacheUtil.class) {
-                    if (!getRedisHelper().exists(key)) {
-                        if (getRedisHelper().setnx(key, String.valueOf(System.currentTimeMillis()))) {
-                            return true;
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            logger.error("getLock", e);
-        }
-        int expires = 1000 * 60 * 3;
-        String currentValue = (String)getRedisHelper().get(key);
-        if (currentValue != null && Long.parseLong(currentValue) < System.currentTimeMillis() - expires) {
-            unlock(key);
-            return getLock(key);
-        }
-        return false;
-    }
+	/** 获取锁 */
+	public static boolean getLock(String key) {
+		try {
+			if (!getRedisHelper().exists(key)) {
+				synchronized (CacheUtil.class) {
+					if (!getRedisHelper().exists(key)) {
+						if (getRedisHelper().setnx(key, String.valueOf(System.currentTimeMillis()))) {
+							return true;
+						}
+					}
+				}
+			}
+		} catch (Exception e) {
+			logger.error("getLock", e);
+		}
+		int expires = 1000 * 60 * 3;
+		String currentValue = (String) getRedisHelper().get(key);
+		if (currentValue != null && Long.parseLong(currentValue) < System.currentTimeMillis() - expires) {
+			if (getRedisHelper().setnx("UNLOCK_" + key, "0")) {
+				unlock(key);
+				getCache().set("UNLOCK_" + key, "0", 1);
+			}
+			return getLock(key);
+		}
+		return false;
+	}
 
-    public static void unlock(String key) {
-        getRedisHelper().unlock(key);
-    }
+	public static void unlock(String key) {
+		getRedisHelper().unlock(key);
+	}
 }
