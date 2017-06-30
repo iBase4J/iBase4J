1475963d58c3ef70de896a8ec1f01d6b7af56aaa
A♂圣旭 <huajieshen@126.com>
2017-06-30 19:40:51 +0800

    时间格式自动适配

--- iBase4J-Common/src/main/java/org/ibase4j/core/support/DateFormat.java
+++ iBase4J-Common/src/main/java/org/ibase4j/core/support/DateFormat.java
@@ -6,10 +6,15 @@ import java.util.Date;
 
 import org.ibase4j.core.util.DateUtil;
 
-
+/**
+ * 时间格式化扩展
+ * 
+ * @author ShenHuaJie
+ * @since 2017年6月30日 下午7:40:00
+ */
 @SuppressWarnings("serial")
 public class DateFormat extends SimpleDateFormat {
-	
+
 	public Date parse(String source) throws ParseException {
 		return DateUtil.stringToDate(source);
 	}
