c1b8c8884c23c25a63dec0805e7d724ac765c2aa
A♂圣旭 <huajieshen@126.com>
2017-08-19 21:37:54 +0800

    缓存优化：Service使用redisson管理数据缓存；Web使用Spring-data-redis管理会话缓存

--- iBase4J-Common/src/main/java/org/ibase4j/core/base/BaseProviderImpl.java
+++ iBase4J-Common/src/main/java/org/ibase4j/core/base/BaseProviderImpl.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.ibase4j.core.Constants;
+import org.ibase4j.core.support.dbcp.ChooseDataSource;
+import org.ibase4j.core.support.dbcp.HandleDataSource;
 import org.ibase4j.core.util.ExceptionUtil;
 import org.ibase4j.core.util.InstanceUtil;
 import org.springframework.beans.BeansException;
@@ -33,6 +35,20 @@ public abstract class BaseProviderImpl implements ApplicationContextAware, BaseP
 			Map<?, ?> map = parameter.getMap();
 			String method = parameter.getMethod();
 			Object[] param = parameter.getParam();
+			try {
+				L: for (String key : ChooseDataSource.METHODTYPE.keySet()) {
+					for (String type : ChooseDataSource.METHODTYPE.get(key)) {
+						if (method.startsWith(type)) {
+							logger.info(key);
+							HandleDataSource.putDataSource(key);
+							break L;
+						}
+					}
+				}
+			} catch (Exception e) {
+				logger.error(e);
+				HandleDataSource.putDataSource("write");
+			}
 			Object result = null;
 			if (param != null) {
 				result = InstanceUtil.invokeMethod(service, method, param);
--- iBase4J-Common/src/main/java/org/ibase4j/core/support/dbcp/HandleDataSource.java
+++ iBase4J-Common/src/main/java/org/ibase4j/core/support/dbcp/HandleDataSource.java
@@ -1,4 +1,4 @@
-package org.ibase4j.core.aspect;
+package org.ibase4j.core.support.dbcp;
 
 /**
  * @author ShenHuaJie
