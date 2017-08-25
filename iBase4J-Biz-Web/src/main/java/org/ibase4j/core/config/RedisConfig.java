cce1bf077554e2b6a5f4f0e8d6bcb3b08a41881b
A♂圣旭 <huajieshen@126.com>
2017-08-25 17:51:57 +0800

    打jar包

--- iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/RedisConfig.java
+++ iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/RedisConfig.java
@@ -2,26 +2,20 @@ package org.ibase4j.core.config;
 
 import java.io.Serializable;
 
-import org.ibase4j.core.listener.SessionListener;
 import org.ibase4j.core.support.cache.RedisHelper;
 import org.ibase4j.core.util.PropertiesUtil;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.session.data.redis.config.annotation.web.http.RedisHttpSessionConfiguration;
-import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import redis.clients.jedis.JedisPoolConfig;
 
-/**
- * 会话管理
- * 
- * @author ShenHuaJie
- * @since 2017年8月14日 上午10:15:44
- */
 @Configuration
-public class SessionConfig {
+public class RedisConfig {
 	@Bean
 	public JedisPoolConfig jedisPoolConfig() {
 		JedisPoolConfig config = new JedisPoolConfig();
@@ -45,28 +39,19 @@ public class SessionConfig {
 	}
 
 	@Bean
-	public RedisHttpSessionConfiguration redisConfig() {
-		RedisHttpSessionConfiguration config = new RedisHttpSessionConfiguration();
-		config.setMaxInactiveIntervalInSeconds(PropertiesUtil.getInt("session.maxInactiveInterval"));
-		config.setRedisNamespace(PropertiesUtil.getString("session.redis.namespace"));
-		return config;
+	public RedisTemplate<Serializable, Serializable> redisTemplate() {
+		RedisTemplate<Serializable, Serializable> redisTemplate = new RedisTemplate<Serializable, Serializable>();
+		StringRedisSerializer keySerializer = new StringRedisSerializer();
+		GenericJackson2JsonRedisSerializer valueSerializer = new GenericJackson2JsonRedisSerializer();
+		redisTemplate.setKeySerializer(keySerializer);
+		redisTemplate.setValueSerializer(valueSerializer);
+		redisTemplate.setHashKeySerializer(keySerializer);
+		redisTemplate.setHashValueSerializer(valueSerializer);
+		return redisTemplate;
 	}
 
 	@Bean
-	public SessionListener sessionListener() {
-		return new SessionListener();
-	}
-
-	@Bean
-	public DefaultCookieSerializer cookie() {
-		DefaultCookieSerializer cookie = new DefaultCookieSerializer();
-		cookie.setCookieName("JSESSIONID");
-		cookie.setCookiePath("/");
-		cookie.setDomainNamePattern("^.+?\\.(\\w+\\.[a-z]+)$");
-		return cookie;
-	}
-
-	@Bean
+	@Qualifier("redisTemplate")
 	public RedisHelper redisHelper(RedisTemplate<Serializable, Serializable> redisTemplate) {
 		RedisHelper redisHelper = new RedisHelper();
 		redisHelper.setRedisTemplate(redisTemplate);
