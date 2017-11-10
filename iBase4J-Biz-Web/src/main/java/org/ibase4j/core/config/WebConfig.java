564ef2075c40d9b033eb7a3ba2649a35981006ba
A♂圣翔 <huajieshen@126.com>
2017-11-10 12:17:31 +0800

    文件上传

--- iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/WebConfig.java
+++ iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/WebConfig.java
@@ -2,10 +2,7 @@ package org.ibase4j.core.config;
 
 import java.util.List;
 
-import javax.servlet.MultipartConfigElement;
-
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
@@ -84,16 +81,6 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 		viewResolver.setViewClass(JstlView.class);
 		return viewResolver;
 	}
-
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        // 单个文件最大
-        factory.setMaxFileSize("10MB"); // KB,MB
-        /// 设置总上传数据总大小
-        factory.setMaxRequestSize("100MB");
-        return factory.createMultipartConfig();
-    }
 
 	@Bean
 	public EventInterceptor eventInterceptor() {
