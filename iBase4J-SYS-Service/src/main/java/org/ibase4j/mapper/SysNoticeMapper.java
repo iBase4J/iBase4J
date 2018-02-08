acf8b63c4c45f78673ee521787c62638a083de6a
A♂圣翔 <huajieshen@126.com>
2018-02-08 14:04:51 +0800

    优化

--- iBase4J-SYS-Service/src/main/java/org/ibase4j/mapper/SysNoticeMapper.java
+++ iBase4J-SYS-Service/src/main/java/org/ibase4j/mapper/SysNoticeMapper.java
@@ -1,8 +1,10 @@
 package org.ibase4j.mapper;
 
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.ibase4j.model.SysNotice;
 
 import top.ibase4j.core.base.BaseMapper;
+import top.ibase4j.core.support.cache.mybatis.EhcacheRedisCache;
 
 /**
  * <p>
@@ -12,6 +14,7 @@ import top.ibase4j.core.base.BaseMapper;
  * @author ShenHuaJie
  * @since 2017-01-29
  */
+@CacheNamespace(implementation = EhcacheRedisCache.class)
 public interface SysNoticeMapper extends BaseMapper<SysNotice> {
 
 }
