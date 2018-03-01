18d53382b11e92407c19a288b92c57ef2b5ff624
A♂圣翔 <huajieshen@126.com>
2018-03-01 18:42:00 +0800

    修改bug

--- iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/WebConfig.java
+++ iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/WebConfig.java
@@ -21,10 +21,10 @@ import org.springframework.web.servlet.view.JstlView;
 import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
+import top.ibase4j.core.base.provider.EventInterceptor;
 import top.ibase4j.core.filter.CsrfFilter;
 import top.ibase4j.core.filter.TokenFilter;
 import top.ibase4j.core.filter.XssFilter;
-import top.ibase4j.core.interceptor.EventInterceptor;
 import top.ibase4j.core.interceptor.LocaleInterceptor;
 import top.ibase4j.core.interceptor.MaliciousRequestInterceptor;
 import top.ibase4j.core.util.InstanceUtil;
