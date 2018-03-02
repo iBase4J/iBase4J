137b01c43cdf1b84a0dc00c634560e999d3f9a1e
A♂圣翔 <huajieshen@126.com>
2018-03-03 00:01:10 +0800

    优化

--- src/main/java/org/ibase4j/core/ServerListener.java
+++ src/main/java/org/ibase4j/core/ServerListener.java
@@ -1,16 +1,22 @@
 package org.ibase4j.core;
 
+import org.ibase4j.service.sys.SysCacheService;
+import org.ibase4j.service.sys.SysDicService;
+import org.ibase4j.service.sys.SysUserService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
-import top.ibase4j.core.Constants;
 import top.ibase4j.core.listener.ApplicationReadyListener;
-import top.ibase4j.core.util.CacheUtil;
 
 @Component
 public class ServerListener extends ApplicationReadyListener {
 	public void onApplicationEvent(ApplicationReadyEvent event) {
-		CacheUtil.getCache().delAll(Constants.MYBATIS_CACHE + "*");
+        ConfigurableApplicationContext context = event.getApplicationContext();
+        context.getBean(SysCacheService.class).flush();
+        context.getBean(SysUserService.class).init();
+        SysDicService sysDicService = context.getBean(SysDicService.class);
+        sysDicService.getAllDic();
 		super.onApplicationEvent(event);
 	}
 }
