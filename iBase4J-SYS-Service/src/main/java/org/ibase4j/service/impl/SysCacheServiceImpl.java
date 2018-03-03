95f71492b631cb6a30eafe7e624764930ca8421b
A♂圣翔 <huajieshen@126.com>
2018-03-03 18:30:17 +0800

    优化

--- iBase4J-SYS-Service/src/main/java/org/ibase4j/service/impl/SysCacheServiceImpl.java
+++ iBase4J-SYS-Service/src/main/java/org/ibase4j/service/impl/SysCacheServiceImpl.java
@@ -12,26 +12,26 @@ import com.weibo.api.motan.config.springsupport.annotation.MotanService;
 import top.ibase4j.core.Constants;
 import top.ibase4j.core.util.CacheUtil;
 
-@Service(interfaceClass=ISysCacheService.class)
-@MotanService(interfaceClass=ISysCacheService.class)
+@Service(interfaceClass = ISysCacheService.class)
+@MotanService(interfaceClass = ISysCacheService.class)
 public class SysCacheServiceImpl implements ISysCacheService {
-    Logger logger = LogManager.getLogger();
+	Logger logger = LogManager.getLogger();
 
-    // 清缓存
-    public void flush() {
-        logger.info("清缓存开始......");
-        CacheUtil.getCache().delAll(Constants.CACHE_NAMESPACE + "*");
-        logger.info("清缓存结束!");
-    }
+	// 清缓存
+	public void flush() {
+		logger.info("清缓存开始......");
+		CacheUtil.getCache().delAll(Constants.CACHE_NAMESPACE + "*");
+		logger.info("清缓存结束!");
+	}
 
-    // 清缓存
-    public void flush(Map<String, String> param) {
-        String key = param.get("key");
-        logger.info("清缓存[{}]开始......", key);
-        CacheUtil.getCache().delAll(Constants.CACHE_NAMESPACE + "*" + key + "*");
-        if (key.contains("Permission")) {
-            CacheUtil.getCache().delAll(Constants.CACHE_NAMESPACE + "*shiro_redis_cache*");
-        }
-        logger.info("清缓存[{}]结束!", key);
-    }
+	// 清缓存
+	public void flush(Map<String, String> param) {
+		String key = param.get("key");
+		logger.info("清缓存[{}]开始......", key);
+		CacheUtil.getCache().delAll(Constants.CACHE_NAMESPACE + "*" + key + "*");
+		if (key.contains("Permission")) {
+			CacheUtil.getCache().delAll(Constants.REDIS_SHIRO_CACHE + "*");
+		}
+		logger.info("清缓存[{}]结束!", key);
+	}
 }
