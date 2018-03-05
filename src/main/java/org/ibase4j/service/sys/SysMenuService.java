9d84f8b57d1d4b04a973aaef9ffe185b7a778ac6
A♂圣翔 <huajieshen@126.com>
2018-03-05 16:32:34 +0800

    优化

--- src/main/java/org/ibase4j/service/sys/SysMenuService.java
+++ src/main/java/org/ibase4j/service/sys/SysMenuService.java
@@ -21,121 +21,89 @@ import top.ibase4j.core.util.InstanceUtil;
 @Service
 @CacheConfig(cacheNames = "sysMenu")
 public class SysMenuService extends BaseService<SysMenu> {
-	@Autowired
-	private SysDicService sysDicService;
+    @Autowired
+    private SysDicService sysDicService;
 
-	public SysMenu queryById(Long id) {
-		SysMenu sysMenu = super.queryById(id);
-		if (sysMenu != null) {
-			if (sysMenu.getParentId() != null && sysMenu.getParentId() != 0) {
-				SysMenu parent = super.queryById(sysMenu.getParentId());
-				if (parent != null) {
-					sysMenu.setParentName(parent.getMenuName());
-				} else {
-					sysMenu.setParentId(null);
-				}
-			}
-		}
-		return sysMenu;
-	}
+    public SysMenu queryById(Long id) {
+        SysMenu sysMenu = super.queryById(id);
+        if (sysMenu != null) {
+            if (sysMenu.getParentId() != null && sysMenu.getParentId() != 0) {
+                SysMenu parent = super.queryById(sysMenu.getParentId());
+                if (parent != null) {
+                    sysMenu.setParentName(parent.getMenuName());
+                } else {
+                    sysMenu.setParentId(null);
+                }
+            }
+        }
+        return sysMenu;
+    }
 
-	public List<SysMenu> queryList(Map<String, Object> params) {
-		params.put("orderBy", "parent_id,sort_no");
-		List<SysMenu> pageInfo = super.queryList(params);
-		Map<String, String> menuTypeMap = sysDicService.queryDicByType("MENUTYPE");
-		Map<Long, Integer> leafMap = InstanceUtil.newHashMap();
-		List<SysMenu> resultList = InstanceUtil.newArrayList();
-		Map<Long, List<SysMenu>> map = InstanceUtil.newHashMap();
-		for (SysMenu sysMenu : pageInfo) {
-			if (sysMenu != null) {
-				if (sysMenu.getMenuType() != null) {
-					sysMenu.setTypeName(menuTypeMap.get(sysMenu.getMenuType().toString()));
-				}
-				if (leafMap.get(sysMenu.getParentId()) == null) {
-					leafMap.put(sysMenu.getParentId(), 0);
-				}
-				leafMap.put(sysMenu.getParentId(), leafMap.get(sysMenu.getParentId()) + 1);
-				if (map.get(sysMenu.getParentId()) == null) {
-					map.put(sysMenu.getParentId(), new ArrayList<SysMenu>());
-				}
-				map.get(sysMenu.getParentId()).add(sysMenu);
-				if (sysMenu.getParentId() == null || sysMenu.getParentId() == 0) {
-					resultList.add(sysMenu);
-				}
-			}
-		}
-		for (SysMenu sysMenu : pageInfo) {
-			if (sysMenu != null) {
-				if (leafMap.get(sysMenu.getId()) != null && leafMap.get(sysMenu.getId()) > 0) {
-					sysMenu.setLeaf(0);
-				}
-				if (map.get(sysMenu.getId()) != null) {
-					resultList.addAll(resultList.indexOf(sysMenu) + 1, map.get(sysMenu.getId()));
-				}
-			}
-		}
+    public List<SysMenu> queryList(Map<String, Object> params) {
+        params.put("orderBy", "parent_id,sort_no");
+        List<SysMenu> pageInfo = super.queryList(params);
+        Map<String, String> menuTypeMap = sysDicService.queryDicByType("MENUTYPE");
+        List<SysMenu> resultList = InstanceUtil.newArrayList();
+        Map<Long, List<SysMenu>> map = InstanceUtil.newHashMap();
+        for (SysMenu sysMenu : pageInfo) {
+            if (sysMenu != null) {
+                if (sysMenu.getMenuType() != null) {
+                    sysMenu.setTypeName(menuTypeMap.get(sysMenu.getMenuType().toString()));
+                }
+                if (sysMenu.getParentId() == null || sysMenu.getParentId() == 0) {
+                    resultList.add(sysMenu);
+                } else {
+                    if (map.get(sysMenu.getParentId()) == null) {
+                        map.put(sysMenu.getParentId(), new ArrayList<SysMenu>());
+                    }
+                    map.get(sysMenu.getParentId()).add(sysMenu);
+                }
+            }
+        }
+        for (SysMenu sysMenu : pageInfo) {
+            if (sysMenu != null) {
+                if (map.get(sysMenu.getId()) != null) {
+                    sysMenu.setLeaf(0);
+                    resultList.addAll(resultList.indexOf(sysMenu) + 1, map.get(sysMenu.getId()));
+                }
+            }
+        }
+        return resultList;
+    }
 
-		return resultList;
-	}
+    public List<Object> queryTreeList(Map<String, Object> params) {
+        List<SysMenu> menuList = queryList(params);
+        Map<String, Object> dicParam = InstanceUtil.newHashMap();
+        dicParam.put("type", "CRUD");
+        dicParam.put("orderBy", "sort_no");
+        List<SysDic> sysDics = sysDicService.queryList(dicParam);
+        List<Object> resultList = InstanceUtil.newArrayList();
+        resultList.addAll(menuList);
+        for (SysMenu sysMenu : menuList) {
+            if (sysMenu != null) {
+                if (sysMenu.getLeaf() != 0) {
+                    List<Map<String, Object>> dicMaps = InstanceUtil.newArrayList();
+                    for (SysDic sysDic : sysDics) {
+                        if (!"read".equals(sysDic.getCode())) {
+                            if ("add".equals(sysDic.getCode()) || "update".equals(sysDic.getCode())
+                                    || "delete".equals(sysDic.getCode())
+                                    || "sys.task.scheduled".equals(sysMenu.getPermission())) {
+                                Map<String, Object> dicMap = InstanceUtil.transBean2Map(sysDic);
+                                dicMap.put("id", "D" + sysDic.getId());
+                                dicMap.put("menuName", sysDic.getCodeText());
+                                dicMap.put("parentId", sysMenu.getId().toString());
+                                dicMaps.add(dicMap);
+                            }
+                        }
+                    }
+                    resultList.addAll(resultList.indexOf(sysMenu) + 1, dicMaps);
+                }
+            }
+        }
+        return resultList;
+    }
 
-	public List<Object> queryTreeList(Map<String, Object> params) {
-		params.put("orderBy", "parent_id,sort_no");
-		List<SysMenu> pageInfo = super.queryList(params);
-		Map<String, String> menuTypeMap = sysDicService.queryDicByType("MENUTYPE");
-		Map<String, Object> dicParam = InstanceUtil.newHashMap();
-		dicParam.put("type", "CRUD");
-		List<SysDic> sysDics = sysDicService.queryList(dicParam);
-		Map<Long, Integer> leafMap = InstanceUtil.newHashMap();
-		List<Object> resultList = InstanceUtil.newArrayList();
-		Map<Long, List<SysMenu>> map = InstanceUtil.newHashMap();
-		for (SysMenu sysMenu : pageInfo) {
-			if (sysMenu != null) {
-				if (sysMenu.getMenuType() != null) {
-					sysMenu.setTypeName(menuTypeMap.get(sysMenu.getMenuType().toString()));
-				}
-				if (leafMap.get(sysMenu.getParentId()) == null) {
-					leafMap.put(sysMenu.getParentId(), 0);
-				}
-				leafMap.put(sysMenu.getParentId(), leafMap.get(sysMenu.getParentId()) + 1);
-				if (map.get(sysMenu.getParentId()) == null) {
-					map.put(sysMenu.getParentId(), new ArrayList<SysMenu>());
-				}
-				map.get(sysMenu.getParentId()).add(sysMenu);
-				if (sysMenu.getParentId() == null || sysMenu.getParentId() == 0) {
-					resultList.add(sysMenu);
-				}
-			}
-		}
-		for (SysMenu sysMenu : pageInfo) {
-			if (sysMenu != null) {
-				if (map.get(sysMenu.getId()) != null) {
-					resultList.addAll(resultList.indexOf(sysMenu) + 1, map.get(sysMenu.getId()));
-				}
-			}
-		}
-		for (SysMenu sysMenu : pageInfo) {
-			if (sysMenu != null) {
-				if (leafMap.get(sysMenu.getId()) != null && leafMap.get(sysMenu.getId()) > 0) {
-					sysMenu.setLeaf(0);
-				} else {
-					List<Map<String, Object>> dicMaps = InstanceUtil.newArrayList();
-					for (SysDic sysDic : sysDics) {
-						if (!"read".equals(sysDic.getCode())) {
-							Map<String, Object> dicMap = InstanceUtil.transBean2Map(sysDic);
-							dicMap.put("id", "D" + sysDic.getId());
-							dicMap.put("menuName", sysDic.getCodeText());
-							dicMap.put("parentId", sysMenu.getId().toString());
-							dicMaps.add(dicMap);
-						}
-					}
-					resultList.addAll(resultList.indexOf(sysMenu) + 1, dicMaps);
-				}
-			}
-		}
-		return resultList;
-	}
-	public List<Map<String, String>> getPermissions() {
-		return ((SysMenuMapper) mapper).getPermissions();
-	}
-
+    public List<Map<String, String>> getPermissions() {
+        return ((SysMenuMapper) mapper).getPermissions();
+    }
 }
