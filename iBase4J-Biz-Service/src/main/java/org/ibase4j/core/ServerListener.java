43d077c2696903db1705392d198f1bee1b6546e0
A♂圣翔 <huajieshen@126.com>
2018-04-18 17:51:13 +0800

    优化

--- iBase4J-Biz-Service/src/main/java/org/ibase4j/core/ServerListener.java
+++ iBase4J-Biz-Service/src/main/java/org/ibase4j/core/ServerListener.java
@@ -6,6 +6,9 @@ import javax.servlet.ServletContextListener;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import com.weibo.api.motan.common.MotanConstants;
+import com.weibo.api.motan.util.MotanSwitcherUtil;
+
 public class ServerListener implements ServletContextListener {
 	protected final Logger logger = LogManager.getLogger(this.getClass());
 
@@ -13,6 +16,7 @@ public class ServerListener implements ServletContextListener {
 	}
 
 	public void contextInitialized(ServletContextEvent contextEvent) {
+        MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
 		logger.info("=================================");
 		logger.info("系统[{}]启动完成!!!", contextEvent.getServletContext().getServletContextName());
 		logger.info("=================================");
