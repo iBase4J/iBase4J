2ee3d5defbdf6564f7f1223c956367f07dcfc4e6
A♂圣翔 <huajieshen@126.com>
2018-04-18 14:27:38 +0800

    运行时情况

--- iBase4J-SYS-Service/src/main/java/org/ibase4j/scheduled/CoreTask.java
+++ iBase4J-SYS-Service/src/main/java/org/ibase4j/scheduled/CoreTask.java
@@ -20,9 +20,15 @@ public class CoreTask {
     private SysSessionService sessionService;
 
     /** 定时清除会话信息 */
-	@Scheduled(cron = "0 0/5 * * * *")
+    @Scheduled(cron = "0 0/5 * * * *")
     public void cleanExpiredSessions() {
         logger.info("cleanExpiredSessions");
         sessionService.cleanExpiredSessions();
+        long mb = 1024 * 1024;
+        Runtime runtime = Runtime.getRuntime();
+        long total = runtime.totalMemory() / mb;
+        long max = runtime.maxMemory() / mb;
+        long free = runtime.freeMemory() / mb;
+        logger.info("最大内存: {}m; 已分配内存: {}m; 已分配内存中的剩余空间: {}m; 最大可用内存: {}m.", max, total, free, max - total + free);
     }
 }
