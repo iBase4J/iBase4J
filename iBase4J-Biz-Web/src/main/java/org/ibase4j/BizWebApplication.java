d3ad6b0e7906a19169a95f48cb789b6d8080fcf8
A♂圣翔 <huajieshen@126.com>
2018-04-22 21:23:03 +0800

    优化；web从删除对mybatis的依赖

--- iBase4J-Biz-Web/src/main/java/org/ibase4j/BizWebApplication.java
+++ iBase4J-Biz-Web/src/main/java/org/ibase4j/BizWebApplication.java
@@ -2,15 +2,14 @@ package org.ibase4j;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * @author ShenHuaJie
+ * @since 2018年4月21日 下午3:30:20
+ */
 @SpringBootApplication(scanBasePackages = {"top.ibase4j", "org.ibase4j"})
 public class BizWebApplication extends SpringBootServletInitializer {
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BizWebApplication.class);
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(BizWebApplication.class, args);
 	}
