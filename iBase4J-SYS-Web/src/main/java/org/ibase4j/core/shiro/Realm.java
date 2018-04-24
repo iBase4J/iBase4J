fa67e60a747ccba7b9bf301640d6e07b31895fdc
A♂圣翔 <huajieshen@126.com>
2018-04-24 16:47:21 +0800

    修改配置

--- iBase4J-SYS-Web/src/main/java/org/ibase4j/core/shiro/Realm.java
+++ iBase4J-SYS-Web/src/main/java/org/ibase4j/core/shiro/Realm.java
@@ -4,6 +4,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.annotation.Resource;
+
 import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -21,12 +23,11 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.subject.Subject;
 import org.ibase4j.model.SysSession;
 import org.ibase4j.model.SysUser;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import org.ibase4j.service.ISysAuthorizeService;
+import org.ibase4j.service.ISysSessionService;
+import org.ibase4j.service.ISysUserService;
 import org.springframework.stereotype.Component;
 
-import top.ibase4j.core.base.provider.IBaseProvider;
-import top.ibase4j.core.base.provider.Parameter;
 import top.ibase4j.core.support.shiro.IRealm;
 import top.ibase4j.core.support.shiro.RedisSessionDAO;
 import top.ibase4j.core.util.SecurityUtil;
@@ -40,98 +41,87 @@ import top.ibase4j.core.util.ShiroUtil;
  */
 @Component
 public class Realm extends AuthorizingRealm implements IRealm {
-	private final Logger logger = LogManager.getLogger();
-	@Autowired
-	@Qualifier("sysProvider")
-	protected IBaseProvider provider;
-	private RedisSessionDAO sessionDAO;
+    private final Logger logger = LogManager.getLogger();
+    @Resource
+    private ISysAuthorizeService sysAuthorizeService;
+    @Resource
+    private ISysUserService sysUserService;
+    @Resource
+    private ISysSessionService sysSessionService;
 
-	public void setSessionDAO(RedisSessionDAO sessionDAO) {
-		this.sessionDAO = sessionDAO;
-	}
+    private RedisSessionDAO sessionDAO;
 
-	// 权限
-	protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-		SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
-		Long userId = (Long) ShiroUtil.getCurrentUser();
-		Parameter parameter = new Parameter("sysAuthorizeService", "queryPermissionByUserId", userId);
-		logger.info("{} execute queryPermissionByUserId start...", parameter.getNo());
-		List<?> list = provider.execute(parameter).getResultList();
-		logger.info("{} execute queryPermissionByUserId end.", parameter.getNo());
-		for (Object permission : list) {
-			if (StringUtils.isNotBlank((String) permission)) {
-				// 添加基于Permission的权限信息
-				info.addStringPermission((String) permission);
-			}
-		}
-		// 添加用户权限
-		info.addStringPermission("user");
-		return info;
-	}
+    public void setSessionDAO(RedisSessionDAO sessionDAO) {
+        this.sessionDAO = sessionDAO;
+    }
 
-	// 登录验证
-	protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken)
-			throws AuthenticationException {
-		UsernamePasswordToken token = (UsernamePasswordToken) authcToken;
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("enable", 1);
-		params.put("account", token.getUsername());
-		Parameter parameter = new Parameter("sysUserService", "queryList", params);
-		logger.info("{} execute sysUserService.queryList start...", parameter.getNo());
-		List<?> list = provider.execute(parameter).getResultList();
-		logger.info("{} execute sysUserService.queryList end.", parameter.getNo());
-		if (list.size() == 1) {
-			SysUser user = (SysUser) list.get(0);
-			StringBuilder sb = new StringBuilder(100);
-			for (int i = 0; i < token.getPassword().length; i++) {
-				sb.append(token.getPassword()[i]);
-			}
-			if (user.getPassword().equals(SecurityUtil.encryptPassword(sb.toString()))) {
-			    ShiroUtil.saveCurrentUser(user.getId());
-				saveSession(user.getAccount(), token.getHost());
-				AuthenticationInfo authcInfo = new SimpleAuthenticationInfo(user.getAccount(), sb.toString(),
-						user.getUserName());
-				return authcInfo;
-			}
-			logger.warn("USER [{}] PASSWORD IS WRONG: {}", token.getUsername(), sb.toString());
-			return null;
-		} else {
-			logger.warn("No user: {}", token.getUsername());
-			return null;
-		}
-	}
+    // 权限
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
+        SimpleAuthorizationInfo info = new SimpleAuthorizationInfo();
+        Long userId = (Long)ShiroUtil.getCurrentUser();
+        List<?> list = sysAuthorizeService.queryPermissionByUserId(userId);
+        for (Object permission : list) {
+            if (StringUtils.isNotBlank((String)permission)) {
+                // 添加基于Permission的权限信息
+                info.addStringPermission((String)permission);
+            }
+        }
+        // 添加用户权限
+        info.addStringPermission("user");
+        return info;
+    }
 
-	/** 保存session */
-	private void saveSession(String account, String host) {
-		// 踢出用户
-		SysSession record = new SysSession();
-		record.setAccount(account);
-		Parameter parameter = new Parameter("sysSessionService", "querySessionIdByAccount", record);
-		logger.info("{} execute querySessionIdByAccount start...", parameter.getNo());
-		List<?> sessionIds = provider.execute(parameter).getResultList();
-		logger.info("{} execute querySessionIdByAccount end.", parameter.getNo());
-		Subject currentUser = SecurityUtils.getSubject();
-		Session session = currentUser.getSession();
-		String currentSessionId = session.getId().toString();
-		if (sessionIds != null) {
-			for (Object sessionId : sessionIds) {
-				record.setSessionId((String) sessionId);
-				parameter = new Parameter("sysSessionService", "deleteBySessionId", record);
-				logger.info("{} execute deleteBySessionId start...", parameter.getNo());
-				provider.execute(parameter);
-				logger.info("{} execute deleteBySessionId end.", parameter.getNo());
-				if (!currentSessionId.equals(sessionId)) {
-					sessionDAO.delete((String) sessionId);
-				}
-			}
-		}
-		// 保存用户
-		record.setSessionId(currentSessionId);
-		record.setIp(StringUtils.isBlank(host) ? session.getHost() : host);
-		record.setStartTime(session.getStartTimestamp());
-		parameter = new Parameter("sysSessionService", "update", record);
-		logger.info("{} execute sysSessionService.update start...", parameter.getNo());
-		provider.execute(parameter);
-		logger.info("{} execute sysSessionService.update end.", parameter.getNo());
-	}
+    // 登录验证
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authcToken)
+        throws AuthenticationException {
+        UsernamePasswordToken token = (UsernamePasswordToken)authcToken;
+        Map<String, Object> params = new HashMap<String, Object>();
+        params.put("enable", 1);
+        params.put("account", token.getUsername());
+        List<?> list = sysUserService.queryList(params);
+        if (list.size() == 1) {
+            SysUser user = (SysUser)list.get(0);
+            StringBuilder sb = new StringBuilder(100);
+            for (int i = 0; i < token.getPassword().length; i++) {
+                sb.append(token.getPassword()[i]);
+            }
+            if (user.getPassword().equals(SecurityUtil.encryptPassword(sb.toString()))) {
+                ShiroUtil.saveCurrentUser(user.getId());
+                saveSession(user.getAccount(), token.getHost());
+                AuthenticationInfo authcInfo = new SimpleAuthenticationInfo(user.getAccount(), sb.toString(),
+                    user.getUserName());
+                return authcInfo;
+            }
+            logger.warn("USER [{}] PASSWORD IS WRONG: {}", token.getUsername(), sb.toString());
+            return null;
+        } else {
+            logger.warn("No user: {}", token.getUsername());
+            return null;
+        }
+    }
+
+    /** 保存session */
+    private void saveSession(String account, String host) {
+        // 踢出用户
+        SysSession record = new SysSession();
+        record.setAccount(account);
+        List<?> sessionIds = sysSessionService.querySessionIdByAccount(record);
+        Subject currentUser = SecurityUtils.getSubject();
+        Session session = currentUser.getSession();
+        String currentSessionId = session.getId().toString();
+        if (sessionIds != null) {
+            for (Object sessionId : sessionIds) {
+                record.setSessionId((String)sessionId);
+                sysSessionService.deleteBySessionId(record);
+                if (!currentSessionId.equals(sessionId)) {
+                    sessionDAO.delete((String)sessionId);
+                }
+            }
+        }
+        // 保存用户
+        record.setSessionId(currentSessionId);
+        record.setIp(StringUtils.isBlank(host) ? session.getHost() : host);
+        record.setStartTime(session.getStartTimestamp());
+        sysSessionService.update(record);
+    }
 }
