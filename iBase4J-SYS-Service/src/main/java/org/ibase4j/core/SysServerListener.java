c0ab72f2d5876e799d004857eeaf53c472227191
A♂圣翔 <huajieshen@126.com>
2018-04-24 17:33:21 +0800

    优化应用监听

--- iBase4J-SYS-Service/src/main/java/org/ibase4j/core/SysServerListener.java
+++ iBase4J-SYS-Service/src/main/java/org/ibase4j/core/SysServerListener.java
@@ -6,9 +6,13 @@ import org.ibase4j.service.ISysCacheService;
 import org.ibase4j.service.ISysDicService;
 import org.ibase4j.service.ISysUserService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.stereotype.Component;
 
+import com.alibaba.dubbo.config.ProtocolConfig;
 import com.weibo.api.motan.common.MotanConstants;
 import com.weibo.api.motan.util.MotanSwitcherUtil;
 
@@ -16,15 +20,21 @@ import top.ibase4j.core.listener.ApplicationReadyListener;
 
 @Component
 public class SysServerListener extends ApplicationReadyListener {
-	protected final Logger logger = LogManager.getLogger(this.getClass());
+    protected final Logger logger = LogManager.getLogger(this.getClass());
 
-	public void onApplicationEvent(ApplicationReadyEvent event) {
-		ConfigurableApplicationContext context = event.getApplicationContext();
-		context.getBean(ISysCacheService.class).flush();
-		context.getBean(ISysUserService.class).init();
-		ISysDicService sysDicService = context.getBean(ISysDicService.class);
-		sysDicService.getAllDic();
-		MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
-		super.onApplicationEvent(event);
-	}
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationReadyEvent) {// 应用已启动完成
+            ConfigurableApplicationContext context = ((ApplicationReadyEvent)event).getApplicationContext();
+            context.getBean(ISysCacheService.class).flush();
+            context.getBean(ISysUserService.class).init();
+            ISysDicService sysDicService = context.getBean(ISysDicService.class);
+            sysDicService.getAllDic();
+            MotanSwitcherUtil.setSwitcherValue(MotanConstants.REGISTRY_HEARTBEAT_SWITCHER, true);
+        } else if (event instanceof ContextStoppedEvent) { // 应用停止
+            ProtocolConfig.destroyAll();
+        } else if (event instanceof ContextClosedEvent) { // 应用关闭
+            ProtocolConfig.destroyAll();
+        }
+        super.onApplicationEvent(event);
+    }
 }
--- iBase4J-SYS-Web/src/main/java/org/ibase4j/core/WebServerListener.java
+++ iBase4J-SYS-Web/src/main/java/org/ibase4j/core/WebServerListener.java
@@ -1,14 +1,22 @@
 package org.ibase4j.core;
 
-import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEvent;
+import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.context.event.ContextStoppedEvent;
 import org.springframework.stereotype.Component;
+
+import com.alibaba.dubbo.config.ProtocolConfig;
 
 import top.ibase4j.core.listener.ApplicationReadyListener;
 
 @Component
 public class WebServerListener extends ApplicationReadyListener {
-
-	public void onApplicationEvent(ApplicationReadyEvent event) {
-		super.onApplicationEvent(event);
-	}
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ContextStoppedEvent) { // 应用停止
+            ProtocolConfig.destroyAll();
+        } else if (event instanceof ContextClosedEvent) { // 应用关闭
+            ProtocolConfig.destroyAll();
+        }
+        super.onApplicationEvent(event);
+    }
 }
