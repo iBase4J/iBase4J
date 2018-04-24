b5b5b634905ecebe2571d6ff03a923006a4da56d
A♂圣翔 <huajieshen@126.com>
2018-04-24 17:35:34 +0800

    优化应用监听

--- src/main/java/org/ibase4j/core/ServerListener.java
+++ src/main/java/org/ibase4j/core/ServerListener.java
@@ -4,6 +4,7 @@ import org.ibase4j.service.sys.SysCacheService;
 import org.ibase4j.service.sys.SysDicService;
 import org.ibase4j.service.sys.SysUserService;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -11,12 +12,14 @@ import top.ibase4j.core.listener.ApplicationReadyListener;
 
 @Component
 public class ServerListener extends ApplicationReadyListener {
-	public void onApplicationEvent(ApplicationReadyEvent event) {
-        ConfigurableApplicationContext context = event.getApplicationContext();
-        context.getBean(SysCacheService.class).flush();
-        context.getBean(SysUserService.class).init();
-        SysDicService sysDicService = context.getBean(SysDicService.class);
-        sysDicService.getAllDic();
-		super.onApplicationEvent(event);
-	}
+    public void onApplicationEvent(ApplicationEvent event) {
+        if (event instanceof ApplicationReadyEvent) {// 应用已启动完成
+            ConfigurableApplicationContext context = ((ApplicationReadyEvent)event).getApplicationContext();
+            context.getBean(SysCacheService.class).flush();
+            context.getBean(SysUserService.class).init();
+            SysDicService sysDicService = context.getBean(SysDicService.class);
+            sysDicService.getAllDic();
+        }
+        super.onApplicationEvent(event);
+    }
 }
