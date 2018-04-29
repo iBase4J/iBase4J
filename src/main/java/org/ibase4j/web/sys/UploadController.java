609d73458ec9fafbf8bd23da67ee801903b2ebe4
A♂圣翔 <huajieshen@126.com>
2018-04-29 11:25:38 +0800

    优化FDFS和日志配置

--- src/main/java/org/ibase4j/web/sys/UploadController.java
+++ src/main/java/org/ibase4j/web/sys/UploadController.java
@@ -1,7 +1,6 @@
 package org.ibase4j.web.sys;
 
 import java.util.List;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,7 +19,7 @@ import top.ibase4j.core.util.UploadUtil;
 
 /**
  * 文件上传控制器
- * 
+ *
  * @author ShenHuaJie
  * @version 2016年5月20日 下午3:11:42
  */
@@ -29,118 +28,115 @@ import top.ibase4j.core.util.UploadUtil;
 @RequestMapping(value = "/upload", method = RequestMethod.POST)
 public class UploadController extends AbstractController {
 
-	public String getService() {
-		return null;
-	}
+    public String getService() {
+        return null;
+    }
 
-	// 上传文件(支持批量)
-	@RequestMapping("/temp/file")
-	@ApiOperation(value = "上传文件")
-	public Object uploadFile(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
-		List<String> fileNames = UploadUtil.uploadFile(request);
-		if (fileNames.size() > 0) {
-			modelMap.put("fileNames", fileNames);
-			return setSuccessModelMap(modelMap);
-		} else {
-			setModelMap(modelMap, HttpCode.BAD_REQUEST);
-			modelMap.put("msg", "请选择要上传的文件！");
-			return modelMap;
-		}
-	}
+    // 上传文件(支持批量)
+    @RequestMapping("/temp/file")
+    @ApiOperation(value = "上传文件")
+    public Object uploadFile(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
+        List<String> fileNames = UploadUtil.uploadFile(request);
+        if (fileNames.size() > 0) {
+            modelMap.put("fileNames", fileNames);
+            return setSuccessModelMap(modelMap);
+        } else {
+            setModelMap(modelMap, HttpCode.BAD_REQUEST);
+            modelMap.put("msg", "请选择要上传的文件！");
+            return modelMap;
+        }
+    }
 
-	// 上传文件(支持批量)
-	@RequestMapping("/temp/image")
-	@ApiOperation(value = "上传图片")
-	public Object uploadImage(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
-		List<String> fileNames = UploadUtil.uploadImage(request, false);
-		if (fileNames.size() > 0) {
-			modelMap.put("fileNames", fileNames);
-			return setSuccessModelMap(modelMap);
-		} else {
-			setModelMap(modelMap, HttpCode.BAD_REQUEST);
-			modelMap.put("msg", "请选择要上传的文件！");
-			return modelMap;
-		}
-	}
+    // 上传文件(支持批量)
+    @RequestMapping("/temp/image")
+    @ApiOperation(value = "上传图片")
+    public Object uploadImage(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
+        List<String> fileNames = UploadUtil.uploadImage(request, false);
+        if (fileNames.size() > 0) {
+            modelMap.put("fileNames", fileNames);
+            return setSuccessModelMap(modelMap);
+        } else {
+            setModelMap(modelMap, HttpCode.BAD_REQUEST);
+            modelMap.put("msg", "请选择要上传的文件！");
+            return modelMap;
+        }
+    }
 
-	// 上传文件(支持批量)
-	@RequestMapping("/temp/imageData")
-	@ApiOperation(value = "上传图片")
-	public Object uploadImageData(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
-		List<String> fileNames = UploadUtil.uploadImageData(request);
-		if (fileNames.size() > 0) {
-			modelMap.put("fileNames", fileNames);
-			return setSuccessModelMap(modelMap);
-		} else {
-			setModelMap(modelMap, HttpCode.BAD_REQUEST);
-			modelMap.put("msg", "请选择要上传的文件！");
-			return modelMap;
-		}
-	}
+    // 上传文件(支持批量)
+    @RequestMapping("/temp/imageData")
+    @ApiOperation(value = "上传图片")
+    public Object uploadImageData(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
+        List<String> fileNames = UploadUtil.uploadImageData(request);
+        if (fileNames.size() > 0) {
+            modelMap.put("fileNames", fileNames);
+            return setSuccessModelMap(modelMap);
+        } else {
+            setModelMap(modelMap, HttpCode.BAD_REQUEST);
+            modelMap.put("msg", "请选择要上传的文件！");
+            return modelMap;
+        }
+    }
 
-	// 上传文件(支持批量)
-	@RequestMapping("/file")
-	@ApiOperation(value = "上传文件")
-	public Object uploadFile2Ftp(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
-		List<String> fileNames = UploadUtil.uploadFile(request);
-		if (fileNames.size() > 0) {
-			List<String> resultList = InstanceUtil.newArrayList();
-			for (int i = 0; i < fileNames.size(); i++) {
-				String filePath = UploadUtil.getUploadDir(request) + fileNames.get(i);
-				String objectId = UUID.randomUUID().toString().replaceAll("-", "");
-				String file = UploadUtil.remove2DFS("file", objectId, filePath).getRemotePath();
-				resultList.add(file);
-			}
-			modelMap.put("fileNames", resultList);
-			return setSuccessModelMap(modelMap);
-		} else {
-			setModelMap(modelMap, HttpCode.BAD_REQUEST);
-			modelMap.put("msg", "请选择要上传的文件！");
-			return modelMap;
-		}
-	}
+    // 上传文件(支持批量)
+    @RequestMapping("/file")
+    @ApiOperation(value = "上传文件")
+    public Object uploadFile2Ftp(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
+        List<String> fileNames = UploadUtil.uploadFile(request);
+        if (fileNames.size() > 0) {
+            List<String> resultList = InstanceUtil.newArrayList();
+            for (int i = 0; i < fileNames.size(); i++) {
+                String filePath = UploadUtil.getUploadDir(request) + fileNames.get(i);
+                String file = UploadUtil.remove2FDFS("file", filePath).getRemotePath();
+                resultList.add(file);
+            }
+            modelMap.put("fileNames", resultList);
+            return setSuccessModelMap(modelMap);
+        } else {
+            setModelMap(modelMap, HttpCode.BAD_REQUEST);
+            modelMap.put("msg", "请选择要上传的文件！");
+            return modelMap;
+        }
+    }
 
-	// 上传文件(支持批量)
-	@RequestMapping("/image")
-	@ApiOperation(value = "上传图片")
-	public Object uploadImage2Ftp(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
-		List<String> fileNames = UploadUtil.uploadImage(request, false);
-		if (fileNames.size() > 0) {
-			List<String> resultList = InstanceUtil.newArrayList();
-			for (int i = 0; i < fileNames.size(); i++) {
-				String filePath = UploadUtil.getUploadDir(request) + fileNames.get(i);
-				String objectId = UUID.randomUUID().toString().replaceAll("-", "");
-				String file = UploadUtil.remove2DFS("image", objectId, filePath).getRemotePath();
-				resultList.add(file);
-			}
-			modelMap.put("fileNames", resultList);
-			return setSuccessModelMap(modelMap);
-		} else {
-			setModelMap(modelMap, HttpCode.BAD_REQUEST);
-			modelMap.put("msg", "请选择要上传的文件！");
-			return modelMap;
-		}
-	}
+    // 上传文件(支持批量)
+    @RequestMapping("/image")
+    @ApiOperation(value = "上传图片")
+    public Object uploadImage2Ftp(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
+        List<String> fileNames = UploadUtil.uploadImage(request, false);
+        if (fileNames.size() > 0) {
+            List<String> resultList = InstanceUtil.newArrayList();
+            for (int i = 0; i < fileNames.size(); i++) {
+                String filePath = UploadUtil.getUploadDir(request) + fileNames.get(i);
+                String file = UploadUtil.remove2FDFS("image", filePath).getRemotePath();
+                resultList.add(file);
+            }
+            modelMap.put("fileNames", resultList);
+            return setSuccessModelMap(modelMap);
+        } else {
+            setModelMap(modelMap, HttpCode.BAD_REQUEST);
+            modelMap.put("msg", "请选择要上传的文件！");
+            return modelMap;
+        }
+    }
 
-	// 上传文件(支持批量)
-	@RequestMapping("/imageData")
-	@ApiOperation(value = "上传图片")
-	public Object uploadImageData2Ftp(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
-		List<String> fileNames = UploadUtil.uploadImageData(request);
-		if (fileNames.size() > 0) {
-			List<String> resultList = InstanceUtil.newArrayList();
-			for (int i = 0; i < fileNames.size(); i++) {
-				String filePath = UploadUtil.getUploadDir(request) + fileNames.get(i);
-				String objectId = UUID.randomUUID().toString().replaceAll("-", "");
-				String file = UploadUtil.remove2DFS("image", objectId, filePath).getRemotePath();
-				resultList.add(file);
-			}
-			modelMap.put("fileNames", resultList);
-			return setSuccessModelMap(modelMap);
-		} else {
-			setModelMap(modelMap, HttpCode.BAD_REQUEST);
-			modelMap.put("msg", "请选择要上传的文件！");
-			return modelMap;
-		}
-	}
+    // 上传文件(支持批量)
+    @RequestMapping("/imageData")
+    @ApiOperation(value = "上传图片")
+    public Object uploadImageData2Ftp(HttpServletRequest request, HttpServletResponse response, ModelMap modelMap) {
+        List<String> fileNames = UploadUtil.uploadImageData(request);
+        if (fileNames.size() > 0) {
+            List<String> resultList = InstanceUtil.newArrayList();
+            for (int i = 0; i < fileNames.size(); i++) {
+                String filePath = UploadUtil.getUploadDir(request) + fileNames.get(i);
+                String file = UploadUtil.remove2FDFS("image", filePath).getRemotePath();
+                resultList.add(file);
+            }
+            modelMap.put("fileNames", resultList);
+            return setSuccessModelMap(modelMap);
+        } else {
+            setModelMap(modelMap, HttpCode.BAD_REQUEST);
+            modelMap.put("msg", "请选择要上传的文件！");
+            return modelMap;
+        }
+    }
 }
