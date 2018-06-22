231fdf093296f5373a5bb3a5cc8a18cad23acc66
A♂圣翔 <huajieshen@126.com>
2018-06-22 18:27:18 +0800

    优化

--- src/main/java/org/ibase4j/service/sys/impl/SysMenuServiceImpl.java
+++ src/main/java/org/ibase4j/service/sys/impl/SysMenuServiceImpl.java
@@ -47,7 +47,6 @@ public class SysMenuServiceImpl extends BaseServiceImpl<SysMenu, SysMenuMapper> 
         params.put("orderBy", "parent_id,sort_no");
         List<SysMenu> pageInfo = super.queryList(params);
         Map<String, String> menuTypeMap = sysDicService.queryDicByType("MENUTYPE");
-        Map<Long, Integer> leafMap = InstanceUtil.newHashMap();
         List<SysMenu> resultList = InstanceUtil.newArrayList();
         Map<Long, List<SysMenu>> map = InstanceUtil.newHashMap();
         for (SysMenu sysMenu : pageInfo) {
@@ -55,82 +54,51 @@ public class SysMenuServiceImpl extends BaseServiceImpl<SysMenu, SysMenuMapper> 
                 if (sysMenu.getMenuType() != null) {
                     sysMenu.setTypeName(menuTypeMap.get(sysMenu.getMenuType().toString()));
                 }
-                if (leafMap.get(sysMenu.getParentId()) == null) {
-                    leafMap.put(sysMenu.getParentId(), 0);
-                }
-                leafMap.put(sysMenu.getParentId(), leafMap.get(sysMenu.getParentId()) + 1);
-                if (map.get(sysMenu.getParentId()) == null) {
-                    map.put(sysMenu.getParentId(), new ArrayList<SysMenu>());
-                }
-                map.get(sysMenu.getParentId()).add(sysMenu);
                 if (sysMenu.getParentId() == null || sysMenu.getParentId() == 0) {
                     resultList.add(sysMenu);
+                } else {
+                    if (map.get(sysMenu.getParentId()) == null) {
+                        map.put(sysMenu.getParentId(), new ArrayList<SysMenu>());
+                    }
+                    map.get(sysMenu.getParentId()).add(sysMenu);
                 }
             }
         }
         for (SysMenu sysMenu : pageInfo) {
             if (sysMenu != null) {
-                if (leafMap.get(sysMenu.getId()) != null && leafMap.get(sysMenu.getId()) > 0) {
-                    sysMenu.setLeaf(0);
-                }
                 if (map.get(sysMenu.getId()) != null) {
+                    sysMenu.setLeaf(0);
                     resultList.addAll(resultList.indexOf(sysMenu) + 1, map.get(sysMenu.getId()));
                 }
             }
         }
-
         return resultList;
     }
 
     @Override
     public List<Object> queryTreeList(Map<String, Object> params) {
-        params.put("orderBy", "parent_id,sort_no");
-        List<SysMenu> pageInfo = super.queryList(params);
-        Map<String, String> menuTypeMap = sysDicService.queryDicByType("MENUTYPE");
+        List<SysMenu> menuList = queryList(params);
         Map<String, Object> dicParam = InstanceUtil.newHashMap();
         dicParam.put("type", "CRUD");
+        dicParam.put("orderBy", "sort_no");
         List<SysDic> sysDics = sysDicService.queryList(dicParam);
-        Map<Long, Integer> leafMap = InstanceUtil.newHashMap();
         List<Object> resultList = InstanceUtil.newArrayList();
-        Map<Long, List<SysMenu>> map = InstanceUtil.newHashMap();
-        for (SysMenu sysMenu : pageInfo) {
+        resultList.addAll(menuList);
+        for (SysMenu sysMenu : menuList) {
             if (sysMenu != null) {
-                if (sysMenu.getMenuType() != null) {
-                    sysMenu.setTypeName(menuTypeMap.get(sysMenu.getMenuType().toString()));
-                }
-                if (leafMap.get(sysMenu.getParentId()) == null) {
-                    leafMap.put(sysMenu.getParentId(), 0);
-                }
-                leafMap.put(sysMenu.getParentId(), leafMap.get(sysMenu.getParentId()) + 1);
-                if (map.get(sysMenu.getParentId()) == null) {
-                    map.put(sysMenu.getParentId(), new ArrayList<SysMenu>());
-                }
-                map.get(sysMenu.getParentId()).add(sysMenu);
-                if (sysMenu.getParentId() == null || sysMenu.getParentId() == 0) {
-                    resultList.add(sysMenu);
-                }
-            }
-        }
-        for (SysMenu sysMenu : pageInfo) {
-            if (sysMenu != null) {
-                if (map.get(sysMenu.getId()) != null) {
-                    resultList.addAll(resultList.indexOf(sysMenu) + 1, map.get(sysMenu.getId()));
-                }
-            }
-        }
-        for (SysMenu sysMenu : pageInfo) {
-            if (sysMenu != null) {
-                if (leafMap.get(sysMenu.getId()) != null && leafMap.get(sysMenu.getId()) > 0) {
-                    sysMenu.setLeaf(0);
-                } else {
+                if (sysMenu.getLeaf() != 0) {
                     List<Map<String, Object>> dicMaps = InstanceUtil.newArrayList();
                     for (SysDic sysDic : sysDics) {
                         if (!"read".equals(sysDic.getCode())) {
-                            Map<String, Object> dicMap = InstanceUtil.transBean2Map(sysDic);
-                            dicMap.put("id", "D" + sysDic.getId());
-                            dicMap.put("menuName", sysDic.getCodeText());
-                            dicMap.put("parentId", sysMenu.getId().toString());
-                            dicMaps.add(dicMap);
+                            if ("add".equals(sysDic.getCode()) || "update".equals(sysDic.getCode())
+                                    || "delete".equals(sysDic.getCode())
+                                    || "sys.task.scheduled".equals(sysMenu.getPermission())) {
+                                Map<String, Object> dicMap = InstanceUtil.transBean2Map(sysDic);
+                                dicMap.put("id", "D" + sysDic.getId());
+                                dicMap.put("menuName", sysDic.getCodeText());
+                                dicMap.put("parentId", sysMenu.getId().toString());
+                                dicMaps.add(dicMap);
+                            }
                         }
                     }
                     resultList.addAll(resultList.indexOf(sysMenu) + 1, dicMaps);
