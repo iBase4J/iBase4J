a8b11a8863b01dea02853f142e9690fd78ed9c5f
A♂圣翔 <huajieshen@126.com>
2018-07-27 11:12:02 +0800

    优化

--- iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/WebConfig.java
+++ iBase4J-Biz-Web/src/main/java/org/ibase4j/core/config/WebConfig.java
@@ -1,15 +1,13 @@
 package org.ibase4j.core.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 import top.ibase4j.core.config.WebMvcConfig;
-import top.ibase4j.core.filter.TokenFilter;
 import top.ibase4j.core.interceptor.EventInterceptor;
-import top.ibase4j.core.interceptor.SignInterceptor;
+import top.ibase4j.core.interceptor.TokenInterceptor;
 
 /**
  * @author ShenHuaJie
@@ -18,15 +16,6 @@ import top.ibase4j.core.interceptor.SignInterceptor;
 @Configuration
 @ComponentScan("org.ibase4j.web")
 public class WebConfig extends WebMvcConfig {
-    @Bean
-    public FilterRegistrationBean<TokenFilter> tokenFilterRegistration() {
-        FilterRegistrationBean<TokenFilter> registration = new FilterRegistrationBean<TokenFilter>(new TokenFilter());
-        registration.setName("tokenFilter");
-        registration.addUrlPatterns("/*");
-        registration.setOrder(5);
-        return registration;
-    }
-
     @Override
     @Bean
     public EventInterceptor eventInterceptor() {
@@ -36,7 +25,7 @@ public class WebConfig extends WebMvcConfig {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         super.addInterceptors(registry);
-        registry.addInterceptor(new SignInterceptor()).addPathPatterns("/**").excludePathPatterns("/*.ico",
+        registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/**").excludePathPatterns("/*.ico",
             "/*/api-docs", "/swagger**", "/swagger-resources/**", "/webjars/**", "/configuration/**");
     }
 }
