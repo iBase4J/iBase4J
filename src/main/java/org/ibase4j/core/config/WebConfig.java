1a45b217a1397080030ffdd9f01b0bc7949f472d
A♂圣翔 <huajieshen@126.com>
2018-07-27 11:46:09 +0800

    优化

--- src/main/java/org/ibase4j/core/config/WebConfig.java
+++ src/main/java/org/ibase4j/core/config/WebConfig.java
@@ -1,29 +1,27 @@
 package org.ibase4j.core.config;
 
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 
 import top.ibase4j.core.config.WebMvcConfig;
-import top.ibase4j.core.filter.TokenFilter;
 import top.ibase4j.core.interceptor.EventInterceptor;
+import top.ibase4j.core.interceptor.TokenInterceptor;
 
 @Configuration
 @ComponentScan({"org.iteachs.web"})
 public class WebConfig extends WebMvcConfig {
-    @Bean
-    public FilterRegistrationBean<TokenFilter> tokenFilterRegistration() {
-        FilterRegistrationBean<TokenFilter> registration = new FilterRegistrationBean<TokenFilter>(new TokenFilter());
-        registration.setName("tokenFilter");
-        registration.addUrlPatterns("/app/*");
-        registration.setOrder(4);
-        return registration;
-    }
-
-
+    @Override
     @Bean
     public EventInterceptor eventInterceptor() {
         return new EventInterceptor();
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        super.addInterceptors(registry);
+        registry.addInterceptor(new TokenInterceptor()).addPathPatterns("/**").excludePathPatterns("/*.ico",
+            "/*/api-docs", "/swagger**", "/swagger-resources/**", "/webjars/**", "/configuration/**");
     }
 }
