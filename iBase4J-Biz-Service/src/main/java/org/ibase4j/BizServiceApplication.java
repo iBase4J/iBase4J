5c1d9c4dc97b5289fcff28d8eff0ff47a868e04c
A♂圣翔 <huajieshen@126.com>
2018-08-10 12:33:55 +0800

    优化

--- iBase4J-Biz-Service/src/main/java/org/ibase4j/BizServiceApplication.java
+++ iBase4J-Biz-Service/src/main/java/org/ibase4j/BizServiceApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import com.alibaba.csp.sentinel.init.InitExecutor;
-import com.alibaba.dubbo.config.spring.context.annotation.DubboComponentScan;
 
-@DubboComponentScan
 @SpringBootApplication(scanBasePackages = {"top.ibase4j", "org.ibase4j"})
 public class BizServiceApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
