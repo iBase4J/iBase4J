2a5c1d88dfa26789f4ff4993e356574aaf7f0d42
A♂圣翔 <huajieshen@126.com>
2018-12-13 10:49:19 +0800

    优化swagger

--- iBase4J-SYS-Web/src/main/java/org/ibase4j/core/config/SwaggerConfig.java
+++ iBase4J-SYS-Web/src/main/java/org/ibase4j/core/config/SwaggerConfig.java
@@ -1,7 +1,11 @@
 package org.ibase4j.core.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.type.AnnotatedTypeMetadata;
 
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -16,18 +20,31 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@Conditional(SwaggerConfig.EanbleSwagger.class)
 public class SwaggerConfig {
 
-	@Bean
-	public Docket platformApi() {
-		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).forCodeGeneration(true);
-	}
+    static class EanbleSwagger implements Condition {
+        @Override
+        public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
+            for (String profile : context.getEnvironment().getActiveProfiles()) {
+                if ("dev".equals(profile) || "test".equals(profile)) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
 
-	private ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("iBase4J-API").description("©2016 Copyright. Powered By iBase4J.")
-				// .termsOfServiceUrl("")
-				.contact(new Contact("iBase4J", "", "iBase4J@163.com")).license("Apache License Version 2.0")
-				.licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE").version("2.0").build();
-	}
+    @Bean
+    public Docket platformApi() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo()).forCodeGeneration(true);
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder().title("iBase4J-API").description("©2016 Copyright. Powered By iBase4J.")
+                // .termsOfServiceUrl("")
+                .contact(new Contact("iBase4J", "", "iBase4J@163.com")).license("Apache License Version 2.0")
+                .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE").version("2.0").build();
+    }
 
 }
