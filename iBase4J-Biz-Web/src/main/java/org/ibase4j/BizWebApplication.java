12f81e9d62fcaa8b1d78e02ae1b1fabed4a41fd0
QQ-star <57897660+QQ-star@users.noreply.github.com>
2021-06-28 23:18:54 +0800

    Update BizWebApplication.java

--- iBase4J-Biz-Web/src/main/java/org/ibase4j/BizWebApplication.java
+++ iBase4J-Biz-Web/src/main/java/org/ibase4j/BizWebApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-
 /**
  * @author ShenHuaJie
  * @since 2018年4月21日 下午3:30:20
